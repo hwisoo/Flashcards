d4059cb499ec080234d2902d87dfed86e6bd71de
Gulzat Karimova and James Cho <gulzat@>
2018-10-16 14:18:41 -0700

    Updated style and finalized the logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,59 @@
 $(document).ready(function(){
-
-
-})
+$("#word1").click(function(){
+    $("#def1").toggleClass("def-show");
+});
+$("#word2").click(function(){
+    $("#def2").toggleClass("def-show");
+});
+$("#word3").click(function(){
+    $("#def3").toggleClass("def-show");
+});
+$("#word4").click(function(){
+    $("#def4").toggleClass("def-show");
+});
+$("#word5").click(function(){
+    $("#def5").toggleClass("def-show");
+});
+$("#word6").click(function(){
+    $("#def6").toggleClass("def-show");
+});
+$("#word7").click(function(){
+    $("#def7").toggleClass("def-show");
+});
+$("#word8").click(function(){
+    $("#def8").toggleClass("def-show");
+});
+$("#word9").click(function(){
+    $("#def9").toggleClass("def-show");
+});
+$("#word10").click(function(){
+    $("#def10").toggleClass("def-show");
+});
+$("#word11").click(function(){
+    $("#def11").toggleClass("def-show");
+});
+$("#word12").click(function(){
+    $("#def12").toggleClass("def-show");
+});
+$("#word13").click(function(){
+    $("#def13").toggleClass("def-show");
+});
+$("#word14").click(function(){
+    $("#def14").toggleClass("def-show");
+});
+$("#word15").click(function(){
+    $("#def15").toggleClass("def-show");
+});
+$("#word16").click(function(){
+    $("#def16").toggleClass("def-show");
+});
+$("#word17").click(function(){
+    $("#def17").toggleClass("def-show");
+});
+$("#word18").click(function(){
+    $("#def18").toggleClass("def-show");
+});
+$("#word19").click(function(){
+    $("#def19").toggleClass("def-show");
+});
+});
